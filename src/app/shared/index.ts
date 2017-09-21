0cac9215035cb8193347d79741b607ff69d93331
Anna Springfield <anna.e.springfield@gmail.com>
2017-09-20 21:38:38 -0500

    add widget service

--- src/app/shared/index.ts
+++ src/app/shared/index.ts
@@ -1,4 +1,5 @@
 export { ItemsService } from './items.service';
+export { WidgetsService } from './widgets.service';
 export { Item } from './item.model';
 export { Widget } from './widget.model';
 
